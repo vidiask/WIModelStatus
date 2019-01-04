d8668f46f3d78688ea029666c57a3c4584b0b38c
Vikas Pandey <vikas.pandey.contractor@bbva.com>
2019-01-04 15:18:32 +0530

    New changes.

---
+++ WIModelStatus/Classes/WIModelStatusView.swift
@@ -0,0 +1,20 @@
+//
+//  WIModelStatusView.swift
+//  WIModelStatus
+//
+//  Created by Vikas Pandey on 04/01/19.
+//
+
+import UIKit
+
+class WIModelStatusView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
